b774ea915208ee984e4c454d91e93919515a2cb1
Mani Qureshi <28346227-maniqureshi264@users.noreply.replit.com>
2026-03-01 10:53:36 +0000

    Day: 5. Adding infrastructure maturity.

--- focusbase-server/index.js
+++ focusbase-server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const connectDB = require('./config/db');
 const cors = require("cors");
 const taskRoutes = require('./routes/taskRoutes');
 const errorHandler = require('./middleware/errorMiddleware');
+
 require("dotenv").config();
 
 
@@ -11,23 +13,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/tasks', taskRoutes);
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+app.use('/api/tasks', taskRoutes);
+
+// Production Awareness
+if (process.env.NODE_ENV === 'production') {
+    console.log('🚀 Running in Production Mode');
+} else {
+    console.log('🛠️ Running in Development Mode');
+}
+
+
 // Global Error Handler must be at the bottom
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch(err => console.log(err));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`📡 Server running on port ${PORT}`);
+});
 
